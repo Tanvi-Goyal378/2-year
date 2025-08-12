90cc70b81251a41680ca03b8d604d695540e6091
Tanvi-goyal378 <tanvi.goyal_cs24@gla.ac.in>
2025-08-12 12:46:04 +0530

    km

---
+++ kmtom.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class kmtom{
+    public static void main(String[] args)
+    {
+        Scanner obj = new Scanner(System.in) ;
+        System.out.print("enter distance");
+        int km=obj.nextInt();
+        double m=km * 0.621371;
+        System.out.println(m);
+    }
+    }
