477092a0df78d1451dd40b6f1d8a655ce4f05510
Tanvi-goyal378 <tanvi.goyal_cs24@gla.ac.in>
2025-08-12 11:56:33 +0530

    .\adding_twonumber.class

---
+++ adding_twonumber.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class adding_twonumber{
+    public static void main(String[] args)
+    { Scanner obj =new Scanner(System.in);
+      System.out.println("enter a");
+      int a=obj.nextInt();
+      System.out.println("enter b");
+      int b=obj.nextInt();
+      System.out.println(a+b);
+    }
+}
