ec5579471c5a4235c6ca97800c7b52b6ed7cdf8f
Tanvi-goyal378 <tanvi.goyal_cs24@gla.ac.in>
2025-08-19 12:16:59 +0530

    sammarks

---
+++ programming_elements/sammarks.java
@@ -0,0 +1,9 @@
+class sammarks{
+    public static void main(String[] args){
+        int maths=94;
+        int physics=95;
+        int chemistry=96;
+        int pcm=(maths+physics+chemistry)/3;
+        System.out.println(" Sam average mark in PCM is "+pcm);
+    }
+}
