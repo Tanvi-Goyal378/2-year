3459deb9af507adf44d1c2bfcdda0d2d15a6b355
Tanvi-goyal378 <tanvi.goyal_cs24@gla.ac.in>
2025-08-19 12:26:21 +0530

    add harry_age.java

---
+++ programming_elements/harry_age.java
@@ -0,0 +1,9 @@
+class harry_age{
+    public static void main(String[] args){
+        int currentyear=2024;
+        int birthyear=2000;
+        int age=currentyear-birthyear;
+        System.out.println(" Harry's age in 2024 is "+age);
+
+    }
+}
