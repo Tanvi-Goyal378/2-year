f8fd54017c7e5c708377992665716669266c2ac6
Tanvi-Goyal378 <tanvi.goyal_cs24@gla.ac.in>
2025-11-10 11:21:33 +0530

    Packages

---
+++ packages/com/inbuiltdemo.java
@@ -0,0 +1,8 @@
+package com.inbuilt;
+ import java.util.*;
+public class inbuiltdemo{
+    public static void main(String[] args){
+	Scanner obj=new Scanner(System.in);
+	double value=obj.nextDouble();
+	System.out.println("The value: "+value);
+}} 
